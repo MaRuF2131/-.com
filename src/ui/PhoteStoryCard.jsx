0e3f3ead7afe162751abe4275c13ff62723f1870
MaRuF2131 <marufahmmed916@gmail.com>
2026-02-28 21:08:48 +0600

    update

--- src/ui/PhoteStoryCard.jsx
+++ src/ui/PhoteStoryCard.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function PhoteStoryCard({photoStory}) {
   return (
     <div className='w-full h-80 pt-2 relative'>
-       <Image src={photoStory?.image || "/default.webp"} alt={photoStory?.title} width={300} height={200} className='object-fill aspect-[5/6.5] rounded-md max-h-80 w-full' />
+       <Image src={photoStory?.image || "/default.webp"} alt={photoStory?.title} width={300} height={200} className='object-fill aspect-[5/6.5] rounded-md max-h-full w-full' />
        <h2 className='absolute bottom-2 w-full text-center text-white font-light  text-2xl'>{photoStory?.title}</h2>
     </div>
   )
--- src/ui/VideoStoryCard.jsx
+++ src/ui/VideoStoryCard.jsx
@@ -5,7 +5,7 @@ function VideoStoryCard({videoStory }) {
   return (
     <div className='w-full h-80 pt-2 relative'>
         <p>{videoStory?.watch}</p>
-        <Image src={videoStory?.image || "/default.webp"} alt={videoStory?.title} width={300} height={200} className='object-fill aspect-[5/6.5] rounded-md max-h-80 w-full' />
+        <Image src={videoStory?.image || "/default.webp"} alt={videoStory?.title} width={300} height={200} className='object-fill aspect-[5/6.5] rounded-md max-h-full w-full' />
 
         <h2 className='absolute bottom-2 w-full text-center text-white font-light  text-2xl'>{videoStory?.title}</h2>           
         {/* overlay */}
