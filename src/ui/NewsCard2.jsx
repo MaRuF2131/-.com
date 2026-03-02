06dfbc74a2ee94a66c574f812470d71e28086d39
MaRuF2131 <marufahmmed916@gmail.com>
2026-03-02 16:00:41 +0600

    update

--- src/ui/NewsCard2.jsx
+++ src/ui/NewsCard2.jsx
@@ -1,14 +1,15 @@
 /* This card is for second news card on home page buttom of NewsCard1 maybe this will be use in other place  */
 
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function NewsCard2({news}) {
   return (
-    <div className='w-full h-full group cursor-pointer  text-black  flex flex-wrap items-center justify-center gap-y-3'>
+    <Link href={`/news/${news?.id}`} className='w-full h-full group cursor-pointer  text-black  flex flex-wrap items-center justify-center gap-y-3'>
         <Image src={news?.image || "/default.webp"} alt={news?.title} width={300} height={200} className='object-fill  rounded-md w-full aspect-[10/6]' />
         <h2 className='text-[22px]  font-semibold  opacity-80 w-full group-hover:text-[#0a58ca]'>{news?.title}</h2>
-    </div>
+    </Link>
   )
 }
 
--- src/ui/NewsCard3.jsx
+++ src/ui/NewsCard3.jsx
@@ -1,16 +1,17 @@
 /* This card is for top  third news card on home page right part of NewsCard1  maybe this will be use in other place  */
 
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 function NewsCard3({news}) {
   return (
-    <div className='group cursor-pointer w-full'>
-        <Image src={news?.image || "/public/default.webp"} alt={news?.title} width={300} height={200} className='w-full h-full object-fill sm:max-h-auto max-h-[240px]' />
+    <Link href={`/news/${news?.id}`} className='group cursor-pointer w-full'>
+        <Image src={news?.image || "/default.webp"} alt={news?.title} width={300} height={200} className='w-full h-full object-fill sm:max-h-auto max-h-[240px]' />
 
         <h2 className='text-3xl font-bold group-hover:text-[#0a58ca]'>{news?.title}</h2>
         <p className='text-lg md:hidden sm:block hidden'>{news?.description.length>200?`${news?.description.slice(0,200)}...`:news?.description}</p>
-    </div>
+    </Link>
   )
 }
 
