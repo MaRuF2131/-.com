3b894519615c88ddde7bec26052dbd9f9be43089
MaRuF2131 <marufahmmed916@gmail.com>
2026-03-04 01:05:29 +0600

    update

--- src/ui/Navbar2.jsx
+++ src/ui/Navbar2.jsx
@@ -31,7 +31,7 @@ function Navbar2() {
            onClick={(e)=>{e.stopPropagation();e.preventDefault(); setOpen(!open)}}
           />
 
-          { open  && <div className='absolute z-[999] top-[89%] sm:left-5 left-0  sm:right-5 right-0 overflow-auto no-scrollbar   bg-white shadow-lg rounded-md  px-5 '>
+          { open  && <div className='absolute z-[999] top-[89%] sm:left-5 left-0 bottom-0 md:bottom-auto  sm:right-5 right-0 overflow-auto no-scrollbar   bg-white shadow-lg rounded-md  px-5 '>
              <h2 className='text-xl border-b border-gray-300 py-2'>
                 {new Date().toLocaleDateString("bn-BD", {
                   weekday: "long",
@@ -39,7 +39,7 @@ function Navbar2() {
                   month: "long",
                   year: "numeric",
                 })}</h2>
-              <div className='flex flex-wrap items-start max-h-screen overflow-auto no-scrollbar justify-start  gap-x-4 gap-y-2 py-4'>
+              <div className='flex flex-wrap items-start   justify-start  gap-x-4 gap-y-2 py-4'>
                 {Object.keys(NavData3).map((key, index) => {
                 const isActive = `${pathname}?message=${NavData3[key].name}` === NavData3[key].link
                 
