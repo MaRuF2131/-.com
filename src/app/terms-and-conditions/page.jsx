d449f213b93cb8578420bf31effc50ece0cbc077
MaRuF2131 <marufahmmed916@gmail.com>
2026-03-05 21:14:43 +0600

    update

---
+++ src/app/terms-and-conditions/page.jsx
@@ -0,0 +1,219 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='p-5 space-y-7 text-base'>
+        <div className='space-y-3'>
+            <p className='text-3xl text-[#0a58ca]'>
+                Terms Of Use
+            </p>
+            <p>
+                We welcome readers and visitors to our "Terms of Use" of Konthoshor and its associated websites, its contents, services and applications. Individuals may access the content in several ways using multiple channels including but not limited to www, digital, social platforms, SMS and RSS feeds using multiple devices including but not limited to computers, mobile phones and PDAs. By using our content and services, that is, by reading or using any content, picture or information whatsoever, the reader/visitor accepts our "Terms of Use” including Konthoshor’s Privacy Policy. If anyone has any objection or reservation to any clause in this "Terms of Use” or the Privacy Policy, she or he may raise the issue with Konthoshor by sending an email at info@Konthoshor.com. However, Konthoshor reserves all right to reject or accept any such objection or reservation. All users of Konthoshor are required to abide by this "Terms of Use”. Failure to comply with the terms may lead to, among others, suspension of account or prohibition from access to the website. By entering the website of Konthoshor or using applications of Konthoshor, readers/visitors are deemed to have received services from Konthoshor. These services include text, audio, video, images, software etc.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Intellectual Property Rights
+            </p>
+            <p>
+                Konthoshor’s content, logos, copyright, trademarks, patents, images, text, graphics, logos, domain names, audio, video and other related intellectual property rights or other features of Konthoshor brand and name belong to Konthoshor or to its licensors. Users cannot claim any rights in and/or our licensor’s intellectual property whether for commercial or non-commercial use. Users are also prevented from making any derivative work from the content of Konthoshor. Infringement of copyright or any other intellectual property of Konthoshor may be sent at info@Konthoshor.com.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Intellectual Property Rights
+            </p>
+            <p>
+                Konthoshor’s content, logos, copyright, trademarks, patents, images, text, graphics, logos, domain names, audio, video and other related intellectual property rights or other features of Konthoshor brand and name belong to Konthoshor or to its licensors. Users cannot claim any rights in and/or our licensor’s intellectual property whether for commercial or non-commercial use. Users are also prevented from making any derivative work from the content of Konthoshor. Infringement of copyright or any other intellectual property of Konthoshor may be sent at info@Konthoshor.com.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Your use of our services
+            </p>
+            <p>
+                Site readers/visitors are required to use Konthoshor services only for lawful means and for read-only purposes. The audio and visual elements of the website or application can only be listened to and viewed and nothing beyond. Konthoshor encourages its readers to share its content(s) in their social media profile, groups and related communities. However, the contents of our services must not be shared with anyone or with any other digital platforms with any modification or alteration. Readers/Visitors are prohibited from hacking the website, or trying to get around our content security setup.
+            </p>
+            <p>
+                The users must use the services only for non-commercial purposes, regardless of whether the person or entity is a commercial entity or not. We grant our users only a license to access and use our services and intellectual property rights subject to the following usage restrictions: users may use available services for personal, private and non-commercial purposes only, the users must not exploit, sell or use any content appearing on our services for any kind of commercial purposes (this does not apply to any user content posted by an individual and in which a visitor/user retains ownership rights), the users must not use provocative or offensive language, pictures or comments targeting the contents of Konthoshor.
+            </p>
+            <p>
+                Taking down contents
+            </p>
+            <p>
+                Konthoshor can take down contents at any time at its sole discretion from its website or application. Readers/Visitors cannot refuse to remove content, games or apps from their respective devices if asked by Konthoshor. This might happen when Konthoshor or its services are taken down.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Unauthorized and prohibited activities
+            </p>
+            <p>
+                The user is specifically required not to associate Konthoshor with any political party, racism, sexism or otherwise damage its reputation. The user is also prohibited from defaming Konthoshor or defaming any other person or entity, or commenting on any court proceedings that may amount to a contempt of court. Harassing, bullying or upsetting the people or any other user is strongly prohibited. The user must not post or upload any image or comment which is offensive or obscure or immoral. Personal attack by way of comment or image is likewise prohibited..
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Protection of Users Device
+            </p>
+            <p>
+                Readers/Visitors are required to take their own precautions and protections in this respect as Konthoshor does not accept any responsibility for any attacks by virus or malware or any other contamination or by anything which has destructive properties. Konthoshor strictly does not hold any responsibility for infection of virus or contamination of your machine or device through your access to any third-party contents. Third party contents may include, but is not limited to Google ads. Any content which is not generated by Konthoshor itself is a third-party content, regardless of whether the content appears on the website of Konthoshor or not.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Prohibition on sharing mark, contents, images, etc.
+
+            </p>
+            <p>
+                Konthoshor prohibits the users from sharing marks, contents or images for whatever purpose, be it commercial or not. When sharing of contents, images or marks are permitted or authorized, then such sharing must be done by attributing the credit and name to Konthoshor in such manner that the attribution is clearly visible when the image or content is generated by Konthoshor. All users are prohibited from taking credit from the contents or images shared, published or generated by Konthoshor.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Redirecting to other Websites
+            </p>
+            <p>
+                Konthoshor will not accept any kind of liability if the user is redirected to any other website including unwanted websites from the Konthoshor.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Third Party Contents
+            </p>
+            <p>
+                Konthoshor does not bear any responsibility or liability whatsoever for any third-party contents. Third party contents include such contents which are not generated or produced by Konthoshor. It includes contents, images and texts which are uploaded or displayed by Konthoshor but which are created or generated or produced by someone or entity other than Konthoshor.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Advertisement
+            </p>
+            <p>
+                The advertisements included in the Konthoshor website and mobile apps, are by third-party companies, which may collect information about users for which Konthoshor shall bear no responsibility that may arise as a result of collecting and/or sharing the information with any other party. Konthoshor shall not accept any liability that may arise as a result of any content of any advertisement that may appear on the Konthoshor website.
+            </p>
+        </div>
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Modification of Terms of Use
+            </p>
+            <p>
+                Konthoshor reserves the right to amend, modify, alter, or omit any terms in the "Terms of Use” at any time but the changed policy shall be immediately uploaded or updated in the website. By continuing to use our services after any changes are made, you accept those changes and will be bound by them. We encourage readers/visitors to periodically check back and review this policy to always know what information we collect, how we use it, and with whom we share it.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Use of Cookies
+            </p>
+            <p>
+                Konthoshor does not collect any user data based on cookies, nor does it store any sort of user information that may be personal to the user. If a third party associated with the Konthoshor website collects user cookies upon your visit to the Konthoshor website, Konthoshor does not control the use of these cookies. Therefore, visitors/users should check the relevant third-party websites. When users register with Konthoshor, personal identity information is collected for authentication. The information Konthoshor collects is not shared with any third party. However, Konthoshor may use the information to send messages, information from Konthoshor or any of its associated companies.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Communication by Konthoshor
+            </p>
+            <p>
+                From time to time, Konthoshor may contact its users via e-mail, phone or SMS for invitation for participation in events, campaigns/competitions, feedback and surveys etc. organized by Konthoshor.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                User Generated Content
+            </p>
+            <p>
+                Users of Konthoshor may submit post and/or upload content (including comments, pictures, videos). In posting content, users confirm that they are the owner or have consent from the owner to post the content and that the content is not obscene, harassing, deceptive, threatening, libelous, invasive of another's privacy, offensive, fraudulent, defamatory of any person or illegal.
+            </p>
+            <p>
+                Konthoshor does not endorse any user generated content nor does it guarantee the accuracy or authority of any user generated content.
+            </p>
+            <p>
+                Furthermore, while using the Konthoshor website readers/visitors agree not to (i) post content which is deliberately intended to upset or harm other users; (ii) use the Konthoshor website to post or otherwise transmit content that victimizes, harasses, degrades, or intimidates an individual or group of individuals on the basis of any impermissible classification, including, without limitation, religion, gender, sexual orientation, race, color, creed, ethnicity, national origin, citizenship, age, marital status, military status or disability; (iii) post or otherwise transmit any content that contains software viruses or any other computer code, files, or programs designed to interrupt, destroy, or limit the functionality of the Konthoshor website or any computer software or hardware or telecommunications equipment; (iv) upload or otherwise transmit any content, or take any other actions with respect to use of the Konthoshor website, that would constitute, or would otherwise encourage, criminal conduct or give rise to civil liability.
+            </p>
+            <p>
+                Konthoshor reserves the right to remove any user’s content, suspend or discontinue one’s opportunity to submit post and/or upload content, at any time and for any reason at its sole discretion without any notice and without further recourse to users.
+            </p>
+            <p>
+                Konthoshor usually filters the content of the website and in the event that any offensive, unpleasant or distasteful comment and/or picture is published regardless of the filtration process Konthoshor shall not accept any liability arising out of it.
+            </p>
+            <p>
+                In the event that users post any comment that may be regarded offensive, degrading, inappropriate or objectionable by any reasonable person or Konthoshor, Konthoshor may use personal information of the concerned to prevent such behavior.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Accessing the website from outside Bangladesh
+            </p>
+            <p>
+                All personal information submitted by users outside Bangladesh will be processed in accordance with these "Terms of Use” and Privacy Policy.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Disclaimer
+            </p>
+            <p>
+                Konthoshor aims to provide its users with the best service. However, it does not and cannot promise that all the information provided within its service including multimedia content like images & videos will always be accurate. The contents provided by Konthoshor are for information purposes only and do not constitute advice. All Konthoshor’s services are provided without any warranties or guarantees.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Posting and Viewing Contents
+            </p>
+            <p>
+                Readers/visitors acknowledge and agree that when they post content on Konthoshor website or view content provided by others, they are doing so at their own discretion and risk, including any reliance they place on the accuracy, completeness, of that content.
+            </p>
+            <p>
+                Some of the information provided by Konthoshor is supplied by Third Parties. Konthoshor has no control over third party content and Konthoshor is unable to guarantee the accuracy of such third-party content. Before relying on any information, whether it is from us or from any third party partner, Konthoshor advises you to verify the accuracy of such information.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Interruption, cross-connection or unavailability of website or application
+            </p>
+            <p>
+                Whilst Konthoshor will do its best to ensure that its service is fully operational at all times, it is not responsible for and shall not be liable to users for any problems or temporary interruptions in using our services arising from factors outside of its control (e.g. technical problems from traffic congestion on the internet) or for any problems arising from participating in or from downloading third party content. To the extent permissible by law, Konthoshor is not responsible for any loss or damage resulting from use of its services or from any content posted in its website.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Governing Law
+            </p>
+            <p>
+                The laws that govern "Terms of Use” of Konthoshor and its relationship with the user is the laws of Bangladesh and any dispute regarding the use, retention, disclosure, leakage or dissemination of the information or date can only be raised in arbitration in accordance with the Arbitration Act, 2001. The place of arbitration shall be Dhaka, Bangladesh and the arbitral tribunal shall consist of three members. The courts of Bangladesh shall have exclusive jurisdiction on this matter. The entire "Terms of Use" shall apply to all who enter the website, receive service or use an application from Konthoshor regardless of their nationality, location, residence or place of business.
+            </p>
+        </div>
+
+        <div className='space-y-3'>
+            <p className='text-xl font-bold'>
+                Opt-out
+            </p>
+            <p>
+                If, at any time, the users prefer not to receive email containing marketing information from us, then the user can simply follow the unsubscribe options at the bottom of each email.
+            </p>
+            <p>
+                If the users no longer wish to have a registered account, the user may terminate the account by sending an email to Konthoshoronline@gmail.com
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default page
