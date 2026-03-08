1909ef7c500062e21e782028aabc4c3f6221b82c
MaRuF2131 <marufahmmed916@gmail.com>
2026-03-08 15:48:59 +0600

    update

--- src/app/[id]/components/LetestNews.jsx
+++ src/app/[id]/components/LetestNews.jsx
@@ -9,6 +9,7 @@ import Image from 'next/image';
 import { FaRegClock } from 'react-icons/fa';
 import NewsTab from '@/components/NewsTab';
 import InnerNavbar from '../ui/InnerNavbar';
+import Link from 'next/link';
 
 function LetestNews({stateMessage }) {
 
@@ -68,27 +69,27 @@ function LetestNews({stateMessage }) {
                 {newsData.map((v,i)=>{
                 if(i%2===0){
                     return(
-                    <div key={i} className='col-span-1'>
+                    <Link href={`/news/${v?._id}?message=${v?.category}`} key={i} className='col-span-1'>
                                     <div key={i} className='group cursor-pointer flex flex-nowrap items-start justify-between w-full  gap-2 sm:border-b border-gray-300 sm:pb-4 pt-2'>
-                                        <Image src={v?.imageUrl || "/default.webp"} alt={v?.title} width={300} height={200} className='sm:w-2/5 w-full max-h-30 sm:max-h-50 rounded-md' />
+                                        <Image src={v?.imageUrl || "/default.webp"} alt={v?.title} width={300} height={200} className='w-2/5  max-h-30 sm:max-h-50 rounded-md' />
                                     <div className='sm:w-auto w-full'>
                                         <h2 className='text-xl font-semibold opacity-80 group-hover:text-[#0a58ca] flex-1'>{v?.title}</h2>
                                         <p className='text-lg opacity-80 hidden sm:inline-flex items-center justify-start gap-x-1'><FaRegClock className="text-lg text-gray-600" />৫ ঘণ্টা আগে</p>
                                     </div>
                                     </div>
-                    </div>
+                    </Link>
                     )
                 }else{
                 return(
-                    <div key={i} className='col-span-1'>
+                    <Link href={`/news/${v?._id}?message=${v?.category}`} key={i} className='col-span-1'>
                                     <div key={i} className='group cursor-pointer flex flex-nowrap items-start justify-between w-full  gap-2 sm:border-b border-gray-300 sm:pb-4 pt-2'>
-                                        <Image src={v?.imageUrl || "/default.webp"} alt={v?.title} width={300} height={200} className='sm:w-2/5 w-full max-h-30 sm:max-h-50 rounded-md' />
+                                        <Image src={v?.imageUrl || "/default.webp"} alt={v?.title} width={300} height={200} className='w-2/5  max-h-30 sm:max-h-50 rounded-md' />
                                     <div className='sm:w-auto w-full'>
                                         <h2 className='text-xl font-semibold opacity-80 group-hover:text-[#0a58ca] flex-1'>{v?.title}</h2>
                                         <p className='text-lg opacity-80 hidden sm:inline-flex items-center justify-start gap-x-1'><FaRegClock className="text-lg text-gray-600" />৫ ঘণ্টা আগে</p>
                                     </div>
                                     </div>
-                    </div>
+                    </Link>
                     )
 
                 }
@@ -105,9 +106,6 @@ function LetestNews({stateMessage }) {
               {/* no data indicator  */}
               {(!hasNextPage && newsData?.length <= 0 && !isFetching && !isFetchingNextPage && status==="success") &&(
                   <NoDataIndicator message="News"></NoDataIndicator>
-              )}
-              {!hasNextPage && data?.pages[0]?.data?.data.length > 0 && (
-                    <FinishIndicator ms={"All News Loaded"}></FinishIndicator>
               )}
         
     </div>
