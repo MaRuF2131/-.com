1620de21a52da28c315907322495d71b8388a330
MaRuF2131 <marufahmmed916@gmail.com>
2026-03-10 02:50:55 +0600

    update

---
+++ src/app/_not-found/page.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">404</h1>
+      <p className="mt-2 text-lg">দুঃখিত, পেজটি পাওয়া যায়নি।</p>
+    </div>
+  );
+}
